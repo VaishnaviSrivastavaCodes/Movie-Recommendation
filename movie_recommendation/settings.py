e16a7abf333c58795e4a7139d36b05e4c4c2b538
Vaishnavi Srivastava <72790971+VaishnaviSrivastavaCodes@users.noreply.github.com>
2021-06-15 20:16:50 +0530

    Can view images

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -15,7 +15,8 @@ from django_heroku import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
@@ -122,9 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
-#     STATIC_DIR,
+#     'static'
 # ]
-STATIC_ROOT = STATIC_DIR
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR),"Movie-Recommendation","static"),
+)
 django_heroku.settings(locals())
